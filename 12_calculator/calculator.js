e7269cdc4aee69072bf58792caecf153526380d1
Rodrigo Gerbasi <31828557+rgerbasi@users.noreply.github.com>
2025-06-02 21:10:14 +0000

    Add 12_calc solution

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,36 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  return arr.reduce((acc, curr) => acc+curr,0)
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+ return arr.reduce((acc, curr) => acc*curr,1)
 };
 
-const power = function() {
-	
+const power = function(num, pow) {
+	if (pow === 0) return 1;
+  let res = num;
+  for (let i = 1; i < pow; i++){
+    res = res * num;
+  }
+  return res;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	if (num <= 1) return 1;
+  let res = 1;
+  while (num > 1){
+    res = res * num;
+    num--;
+  }
+  return res;
 };
 
 // Do not edit below this line
