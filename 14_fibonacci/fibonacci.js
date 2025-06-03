5a65e437a57e685baba2ae1343b99b0c01cbc980
Rodrigo Gerbasi <31828557+rgerbasi@users.noreply.github.com>
2025-06-03 19:30:35 +0000

    add: 14 fibonacci soln

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,4 +1,8 @@
-const fibonacci = function() {
+const fibonacci = function(number) {
+    if (number < 0) return "OOPS"
+    if (number == 0) return 0;
+    if (number == 1) return 1;
+    return fibonacci(number-1) + fibonacci(number-2)
 
 };
 
