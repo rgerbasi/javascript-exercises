9c0fe621e4b06f448d45490ea055e58a6362984b
Rodrigo Gerbasi <31828557+rgerbasi@users.noreply.github.com>
2025-06-03 17:17:40 +0000

    add: palindrome sol

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,9 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    let cleaned = str
+        .toLowerCase()
+        .replace(/[^a-z0-9]/g, "");
+    let reversed = cleaned.split("").reverse().join("")
+    return reversed === cleaned
 };
 
 // Do not edit below this line
