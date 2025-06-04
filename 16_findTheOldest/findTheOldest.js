7e4b213fa5400ddf63756c186ef2899bae652918
Rodrigo Gerbasi <31828557+rgerbasi@users.noreply.github.com>
2025-06-04 00:22:17 +0000

    add: 16 soln find oldest

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,4 +1,26 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+
+    let res = people.reduce( (acc, curr) => {
+        let thisYear = new Date().getFullYear()
+        let age1 = (
+            "yearOfDeath" in acc ?
+            acc.yearOfDeath - acc.yearOfBirth
+            :
+            thisYear - acc.yearOfBirth
+        );
+        let age2 = (
+            "yearOfDeath" in curr ?
+            curr.yearOfDeath - curr.yearOfBirth
+            :
+            thisYear - curr.yearOfBirth
+        );
+
+        if (age1 < age2){
+            return curr;
+        }   
+        return acc;
+    });
+    return res;
 
 };
 
